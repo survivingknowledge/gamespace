0084c70d9e54d6e8190005847f2df10aa6f11134
Jordan Sleeper <survivingknowledge@gmail.com>
2017-06-10 19:58:24 -0400

    article title heading area and floating horizontal rule

--- src/components/articles/Articles.js
+++ src/components/articles/Articles.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 
+import FontAwesome from 'react-fontawesome';
+
 class Articles extends Component {
 
   render(){
     return (
       <div className='articles'>
-        much articles
+        <div className='article-title-area'>
+          <h3>Latest Articles</h3>
+          <div className='articles-see-all-link'>
+            <a href=''>See All Articles<FontAwesome name='angle-right' /></a>
+          </div>
+          <div className='horz-rule'></div>
+        </div>
       </div>
     );
   }
