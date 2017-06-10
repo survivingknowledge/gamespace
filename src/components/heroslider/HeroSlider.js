2eb82f848e84a0c61f9d2fdb189a1da410063a11
Jordan Sleeper <survivingknowledge@gmail.com>
2017-06-10 18:50:35 -0400

    gradient overlay, positioned title, background img auto 200px for mobile

--- src/components/heroslider/HeroSlider.js
+++ src/components/heroslider/HeroSlider.js
@@ -6,12 +6,45 @@ import RightControl from './RightControl';
 
 class HeroSlider extends Component {
 
+  constructor(props){
+    super(props);
+
+    this.state = {
+      slides: [],
+      currentSlide: 0
+    }
+
+    this.fetchSlides = this.fetchSlides.bind(this);
+  }
+
+  fetchSlides(){
+    //fetch slides in json format
+    const slides = [
+      {
+        title: 'ARMS Review',
+        content: 'some awesome content',
+        img_url: 'img/heroslider/arms1-1170x450.jpg',
+        href_url: '/'
+      }
+    ];
+
+    this.setState({
+      slides: slides
+    });
+  }
+
+  componentWillMount(){
+    this.fetchSlides();
+  }
+
   render(){
+    const { slides, currentSlide } = this.state;
+
     return (
       <div className='hero-slider'>
+        <div className='hero-slider-overlay'></div>
         <LeftControl />
-        <Slide text={"slide1"} size="2x"/>
-        <Slide text={"slide222222"}/>
+        <Slide slide={slides[currentSlide]}/>
         <RightControl />
       </div>
     );
--- src/components/heroslider/Slide.js
+++ src/components/heroslider/Slide.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Slide = (props) => {
+const Slide = ({
+  slide
+}) => {
+  const style = {
+    background: `url('${slide.img_url}')`
+  }
+
   return (
-    <div className='slide'>
-      {props.text}
+    <div className='slide' style={style}>
+      <div className='slide-title'>
+        <a href=''>{slide.title}</a>
+      </div>
     </div>
   );
 }
